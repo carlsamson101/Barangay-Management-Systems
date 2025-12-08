96eb2bfa0197b220bf191df71d21e9f9723d9290
Carl Joseph Samson <carljoseph.samson@g.msuiit.edu.ph>
2025-12-08 21:24:08 +0800

    deploy

--- rwm-system/lib/api.ts
+++ rwm-system/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+baseURL: process.env.EXPO_PUBLIC_API_URL,
   headers: { "Content-Type": "application/json" },
 });
 
